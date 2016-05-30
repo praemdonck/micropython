49cbdee495be0b874819227b96e596c91cf4d693
Pablo Raemdonck <praemdonck@gmail.com>
2016-05-30 23:26:30 +0100

    Fixed ccs launch scripts and started working on port

--- minimal/main.c
+++ minimal/main.c
@@ -181,17 +181,51 @@ typedef struct {
     volatile uint32_t APB2ENR;
 } periph_rcc_t;
 
+//typedef struct {
+//    volatile uint32_t MODER;
+//    volatile uint32_t OTYPER;
+//    volatile uint32_t OSPEEDR;
+//    volatile uint32_t PUPDR;
+//    volatile uint32_t IDR;
+//    volatile uint32_t ODR;
+//    volatile uint16_t BSRRL;
+//    volatile uint16_t BSRRH;
+//    volatile uint32_t LCKR;
+//    volatile uint32_t AFR[2];
+//} periph_gpio_t;
+
+
 typedef struct {
-    volatile uint32_t MODER;
-    volatile uint32_t OTYPER;
-    volatile uint32_t OSPEEDR;
-    volatile uint32_t PUPDR;
-    volatile uint32_t IDR;
-    volatile uint32_t ODR;
-    volatile uint16_t BSRRL;
-    volatile uint16_t BSRRH;
-    volatile uint32_t LCKR;
-    volatile uint32_t AFR[2];
+    volatile uint32_t _1[255];        // 0x40004000 - 0x400043FB 
+    volatile uint32_t DATA;           // 0x400043FC - 0x400043FF 
+    volatile uint32_t DIR;            // 0x40004400 - 0x40004403 
+    volatile uint32_t IS;             // 0x40004404 - 0x40004407 
+    volatile uint32_t IBE;            // 0x40004408 - 0x4000440B 
+    volatile uint32_t IEV;            // 0x4000440C - 0x4000440F
+    volatile uint32_t IM;             // 0x40004410 - 0x40004413
+    volatile uint32_t RIS;            // 0x40004414 - 0x40004417
+    volatile uint32_t MIS;            // 0x40004418 - 0x4000441B
+    volatile uint32_t ICLR;           // 0x4000441C - 0x4000441F
+    volatile uint32_t AFSEL;          // 0x40004420 - 0x40004423
+
+    volatile uint32_t _2[55];         // 0x40004424 - 0x400044FF 
+
+    volatile uint32_t DR2R;           // 0x40004500 - 0x40004503
+    volatile uint32_t DR4R;           // 0x40004504 - 0x40004507
+    volatile uint32_t DR8R;           // 0x40004508 - 0x4000450B
+    volatile uint32_t ODR;            // 0x4000450C - 0x4000450F
+    volatile uint32_t PUR;            // 0x40004510 - 0x40004513
+    volatile uint32_t PDR;            // 0x40004514 - 0x40004517
+    volatile uint32_t SLR;            // 0x40004518 - 0x4000451B
+    volatile uint32_t DEN;            // 0x4000451C - 0x4000451F
+    volatile uint32_t LOCK;           // 0x40004520 - 0x40004523
+    volatile uint32_t CR;             // 0x40004524 - 0x40004527
+    volatile uint32_t AMSEL;          // 0x40004528 - 0x4000452B
+    volatile uint32_t PCTL;           // 0x4000452C - 0x4000452F
+    volatile uint32_t ADCCTL;         // 0x40004530 - 0x40004533
+    volatile uint32_t DMACTL;         // 0x40004534 - 0x40004537
+//    volatile uint32_t AMSEL;          // 0x40004538 - 0x4000453B
+//    volatile uint32_t PCTL;           // 0x4000453C - 0x4000453F
 } periph_gpio_t;
 
 typedef struct {
@@ -202,8 +236,13 @@ typedef struct {
 } periph_uart_t;
 
 #define USART1 ((periph_uart_t*) 0x40011000)
-#define GPIOA  ((periph_gpio_t*) 0x40020000)
-#define GPIOB  ((periph_gpio_t*) 0x40020400)
+#define GPIOA  ((periph_gpio_t*) 0x40004000)
+#define GPIOB  ((periph_gpio_t*) 0x40005000)
+
+
+#define RCGC2  ((unsigned int*)  0x400FE108)
+
+
 #define RCC    ((periph_rcc_t*)  0x40023800)
 
 // simple GPIO interface
@@ -214,41 +253,42 @@ typedef struct {
 #define GPIO_PULL_UP (0)
 #define GPIO_PULL_DOWN (1)
 void gpio_init(periph_gpio_t *gpio, int pin, int mode, int pull, int alt) {
-    gpio->MODER = (gpio->MODER & ~(3 << (2 * pin))) | (mode << (2 * pin));
-    // OTYPER is left as default push-pull
-    // OSPEEDR is left as default low speed
-    gpio->PUPDR = (gpio->PUPDR & ~(3 << (2 * pin))) | (pull << (2 * pin));
-    gpio->AFR[pin >> 3] = (gpio->AFR[pin >> 3] & ~(15 << (4 * (pin & 7)))) | (alt << (4 * (pin & 7)));
+//    gpio->MODER = (gpio->MODER & ~(3 << (2 * pin))) | (mode << (2 * pin));
+//    // OTYPER is left as default push-pull
+//    // OSPEEDR is left as default low speed
+//    gpio->PUPDR = (gpio->PUPDR & ~(3 << (2 * pin))) | (pull << (2 * pin));
+//    gpio->AFR[pin >> 3] = (gpio->AFR[pin >> 3] & ~(15 << (4 * (pin & 7)))) | (alt << (4 * (pin & 7)));
 }
-#define gpio_get(gpio, pin) ((gpio->IDR >> (pin)) & 1)
-#define gpio_set(gpio, pin, value) do { gpio->ODR = (gpio->ODR & ~(1 << (pin))) | (value << pin); } while (0)
-#define gpio_low(gpio, pin) do { gpio->BSRRH = (1 << (pin)); } while (0)
-#define gpio_high(gpio, pin) do { gpio->BSRRL = (1 << (pin)); } while (0)
+#define gpio_get(gpio, pin) ((gpio->DATA >> (pin)) & 1)
+#define gpio_set(gpio, pin, value) do { gpio->DATA = (gpio->DATA & ~(1 << (pin))) | (value << pin); } while (0)
+#define gpio_low(gpio, pin) do { gpio->DATA = (gpio->DATA & ~(1 << (pin))); } while (0)
+#define gpio_high(gpio, pin) do { gpio->DATA = (gpio->DATA | (1 << (pin))); } while (0)
 
 void stm32_init(void) {
     // basic MCU config
-    RCC->CR |= (uint32_t)0x00000001; // set HSION
-    RCC->CFGR = 0x00000000; // reset all
-    RCC->CR &= (uint32_t)0xfef6ffff; // reset HSEON, CSSON, PLLON
-    RCC->PLLCFGR = 0x24003010; // reset PLLCFGR
-    RCC->CR &= (uint32_t)0xfffbffff; // reset HSEBYP
-    RCC->CIR = 0x00000000; // disable IRQs
+    //RCC->CR |= (uint32_t)0x00000001; // set HSION
+    //RCC->CFGR = 0x00000000; // reset all
+    //RCC->CR &= (uint32_t)0xfef6ffff; // reset HSEON, CSSON, PLLON
+    //RCC->PLLCFGR = 0x24003010; // reset PLLCFGR
+    //RCC->CR &= (uint32_t)0xfffbffff; // reset HSEBYP
+    //RCC->CIR = 0x00000000; // disable IRQs
 
-    // leave the clock as-is (internal 16MHz)
+    //// leave the clock as-is (internal 16MHz)
 
-    // enable GPIO clocks
-    RCC->AHB1ENR |= 0x00000003; // GPIOAEN, GPIOBEN
+    //// enable GPIO clocks
+    //RCC->AHB1ENR |= 0x00000003; // GPIOAEN, GPIOBEN
+    *RCGC2 |= 0x03; 
 
-    // turn on an LED! (on pyboard it's the red one)
-    gpio_init(GPIOA, 13, GPIO_MODE_OUT, GPIO_PULL_NONE, 0);
-    gpio_high(GPIOA, 13);
+    //// turn on an LED! (on pyboard it's the red one)
+    //gpio_init(GPIOA, 13, GPIO_MODE_OUT, GPIO_PULL_NONE, 0);
+    //gpio_high(GPIOA, 13);
 
-    // enable UART1 at 9600 baud (TX=B6, RX=B7)
-    gpio_init(GPIOB, 6, GPIO_MODE_ALT, GPIO_PULL_NONE, 7);
-    gpio_init(GPIOB, 7, GPIO_MODE_ALT, GPIO_PULL_NONE, 7);
-    RCC->APB2ENR |= 0x00000010; // USART1EN
-    USART1->BRR = (104 << 4) | 3; // 16MHz/(16*104.1875) = 9598 baud
-    USART1->CR1 = 0x0000200c; // USART enable, tx enable, rx enable
+    //// enable UART1 at 9600 baud (TX=B6, RX=B7)
+    //gpio_init(GPIOB, 6, GPIO_MODE_ALT, GPIO_PULL_NONE, 7);
+    //gpio_init(GPIOB, 7, GPIO_MODE_ALT, GPIO_PULL_NONE, 7);
+    //RCC->APB2ENR |= 0x00000010; // USART1EN
+    //USART1->BRR = (104 << 4) | 3; // 16MHz/(16*104.1875) = 9598 baud
+    //USART1->CR1 = 0x0000200c; // USART enable, tx enable, rx enable
 }
 
 #endif
